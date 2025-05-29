b8f50f33326843253667f72e556efaf117e217bb
dauphinYan <584485321@qq.com>
2025-05-29 10:45:25 +0800

    新增角色描边。

--- Source/GAS_Aura/Public/Character/AuraEnemy.h
+++ Source/GAS_Aura/Public/Character/AuraEnemy.h
@@ -9,11 +9,14 @@ UCLASS()
 class GAS_AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
+
+public:
+	AAuraEnemy();
+
 public:
 	virtual void HighlightActor() override;
 
 	virtual void UnHighlightActor() override;
 
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+
 };
