8a10f26727c4320ffe2dad3642e19e8606ffd43c
dauphinYan <584485321@qq.com>
2025-06-09 12:30:41 +0800

    PreAttributeChange.

--- Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -21,6 +21,8 @@ public:
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
 public:
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
